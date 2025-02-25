11266c22ac9adbe28c9ad867563bee5757c21ab0
rickramen <rickammann99@gmail.com>
2025-02-25 15:43:51 -0800

    initial setup, webpack

---
+++ webpack.common.js
@@ -0,0 +1,28 @@
+// webpack.common.js
+
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      filename: "index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
---
+++ webpack.dev.js
@@ -0,0 +1,14 @@
+// webpack.dev.js
+
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+
+module.exports = merge(common, {
+  mode: "development",
+  devtool: "eval-source-map",
+  devServer: {
+    static: "./public",
+    open: true,
+    hot: true,
+  },
+});
